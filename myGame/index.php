826ed7c16122e3f0a68493aac50a4000f086bfa0
Plotnikov Aleksey <plotnik97@mail.ru>
2019-06-06 20:58:40 +0300

    Update index.php

--- myGame/index.php
+++ myGame/index.php
@@ -1,10 +1,3 @@
-<?php
-if(isset($_POST['back'])){
-    header("Location: {$_SERVER['HTTP_REFERER']}");
-  }
-?>
-
-
 <html>
        <head>
               <title>platformerHTML</title>
@@ -19,10 +12,6 @@ if(isset($_POST['back'])){
               <a class="superdev" href="javascript:%7B%20window.__gwt_bookmarklet_params%20%3D%20%7B'server_url'%3A'http%3A%2F%2Flocalhost%3A9876%2F'%7D%3B%20var%20s%20%3D%20document.createElement('script')%3B%20s.src%20%3D%20'http%3A%2F%2Flocalhost%3A9876%2Fdev_mode_on.js'%3B%20void(document.getElementsByTagName('head')%5B0%5D.appendChild(s))%3B%7D">&#8635;</a>
               <div align="center" id="embed-html"></div>
               <script type="text/javascript" src="html/html.nocache.js"></script>
-
-              <form action="" id="buttonform" method="post"name="loginform">
-                <p class="submit"><input class="button" name="back"type= "submit" value="НАЗАД"></p>
-              </form>
        </body>
 
        <script>
